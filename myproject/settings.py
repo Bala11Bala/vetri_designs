5f98a28d68e3c3ea73ca33cac40094cc11616467
balakannan1123 <balamakesh2001@gmail.com>
2025-10-10 11:26:37 +0530

    Configured static files for Render

--- myproject/settings.py
+++ myproject/settings.py
@@ -132,14 +132,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 import os
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Static files
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'myapp/static']
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myapp/static')]
-
+# Media files (if you upload images)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
